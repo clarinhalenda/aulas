d0a213de8d023125b32346c0aacf6f76731cb00e
clarinhalenda <100954083+clarinhalenda@users.noreply.github.com>
2022-03-25 13:14:08 -0300

    Create codigo.ino

---
+++ exercicios/ex2/codigo.ino
@@ -0,0 +1,22 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+ pinMode(8, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(500); // Wait for 10000 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(500); // Wait for 10000 millisecond(s)
+  
+  digitalWrite(8, HIGH);
+  delay (500);//Wait for 10000 millisecond(s) 
+  digitalWrite(8, LOW);
+  delay(500); // Wait for 10000 millisecond(s)
+  
+  
+} 
